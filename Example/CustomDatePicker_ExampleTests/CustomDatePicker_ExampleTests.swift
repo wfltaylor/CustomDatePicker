5ae2bfc86848de2e1140f85e1f527fbdacd5cec6
wfltaylor <wfltaylor@gmail.com>
2018-06-17 14:53:15 +1000

    Configured Testing

---
+++ Example/CustomDatePicker_ExampleTests/CustomDatePicker_ExampleTests.swift
@@ -0,0 +1,27 @@
+//
+//  CustomDatePicker_ExampleTests.swift
+//  CustomDatePicker_ExampleTests
+//
+//  Created by William Taylor on 17/6/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class CustomDatePicker_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    
+    
+    
+}
